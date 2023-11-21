fdfcf1756b8efb56fb9e34437fa4bb6f5b779422
jluker <josephsluker@gmail.com>
2023-11-21 03:55:53 -0700

    setting up

--- verden-frontend/svelte.config.js
+++ verden-frontend/svelte.config.js
@@ -11,8 +11,27 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+		adapter: adapter(),
+
+
+
+	},
+			// Add Vite configurations specific for Docker here
+			vite: {
+				server: {
+					hmr: {
+						// You might need to configure these options
+						// depending on your Docker setup
+						protocol: 'ws',
+						host: 'localhost',
+						port: 3000,
+						// Uncomment the following line to use polling
+						// watch: {
+						//     usePolling: true,
+						// },
+					},
+				},
+			},
 };
 
 export default config;
